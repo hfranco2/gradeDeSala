d41fb037c09871a8178f87345b20a20de4f9f66c
hfranco2 <francohenrique10@gmail.com>
2021-11-16 19:59:34 -0300

    eliminando arquivos

---
+++ grade react/src/CalendarHeader/CalendarHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export const CalendarHeader = () =>{
+     return(
+     <div id="header">
+     <div id="monthDisplay"></div>
+     <div>
+       <h1>Grade de Salas</h1>
+     </div>
+     
+   </div>
+   );
+}
---
+++ grade react/src/Day/Day1.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+export const Day1 = ({ day, onClick }) => {
+  const className = `day1`;
+  const id = `2div${day.id}`
+  return (
+        <div onClick={onClick} className={className} id={id}>
+      {day.id}
+
+      {day.event1 && <div className='event'>{day.event1.title}</div>}
+    </div>
+  );
+};
+ 
+ 
+ 
