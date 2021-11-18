6f3ccc86caa2a1a247babc79ac72dd964f1059bc
hfranco2 <francohenrique10@gmail.com>
2021-11-18 13:34:29 -0300

    barra lateral criada

--- grade react/src/App/App.jsx
+++ grade react/src/App/App.jsx
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { CalendarHeader } from '../CalendarHeader/CalendarHeader';
-import { Day } from '../Day';
-import { Day1} from '../Day/Day1';
+import { Day } from '../Day/Day';
 import { NewEventModal } from '../NewEventModal';
 import { DeleteEventModal } from '../DeleteEventModal';
 
@@ -9,11 +8,6 @@ export const App = () => {
     
     
     const[days, setDays] = useState([]);
-    const[days1, setDays1] = useState([]);
-    const[days2, setDays2] = useState([]);
-    const[days3, setDays3] = useState([]);
-    const[days4, setDays4] = useState([]);
-    const[days5, setDays5] = useState([]);
     const[dateDisplay, setDateDisplay] = useState('');
     const [clicked, setClicked] = useState();
 
@@ -21,24 +15,19 @@ export const App = () => {
         localStorage.getItem('events') ?
          JSON.parse(localStorage.getItem('events')) : []
     );
-    const [events1,setEvents1] = useState(
-      localStorage.getItem('events1') ?
-       JSON.parse(localStorage.getItem('events1')) : []
-  );
+
     const eventForDate = id => events.find(e => e.id === id)
     useEffect(() =>{
         localStorage.setItem('events', JSON.stringify(events));
     },[events]);
-    
 
     useEffect(() => {
-    //   const dt = new Date();
-    //   const year = dt.getFullYear();
+      const dt = new Date();
+      const year = dt.getFullYear();
       
-    //  setDateDisplay( `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`);
+     setDateDisplay( `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`);
      const daysArr =[];
-    //  const days1Arr= [];
-     for(let i = 1; i<45;i++){
+     for(let i = 1; i<=390;i++){
       daysArr.push({
         value: 'div'+i,
         event: eventForDate(i),
@@ -49,90 +38,67 @@ export const App = () => {
      setDays(daysArr)
     },[events]);
 
-    const eventForDate1 = id => events.find(e => e.id === id)
-    useEffect(() =>{
-        localStorage.setItem('events', JSON.stringify(events));
-    },[events]);
-
-    useEffect(() => {
-       const days1Arr =[];
-       for(let i = 1; i<45;i++){
-        days1Arr.push({
-          value: 'div'+i,
-          event: eventForDate1(i),
-          id: i,
-          
-        });
-       }
-       setDays1(days1Arr)
-      },[events1]);
-    //   useEffect(() => {
-    //     const daysArr2 =[];
-    //     for(let i = 1; i<45;i++){
-    //      daysArr2.push({
-    //        value: 'div'+i,
-    //        event: eventForDate(i),
-    //        id: i,
-           
-    //      });
-    //     }
-    //     setDays(daysArr2)
-    //    },[events]);
-    //    useEffect(() => {
-    //     const daysArr3 =[];
-    //     for(let i = 1; i<45;i++){
-    //      daysArr3.push({
-    //        value: 'div'+i,
-    //        event: eventForDate(i),
-    //        id: i,
-           
-    //      });
-    //     }
-    //     setDays(daysArr3)
-    //    },[events]);
-    //    useEffect(() => {
-    //     const daysArr4 =[];
-    //     for(let i = 1; i<45;i++){
-    //      daysArr4.push({
-    //        value: 'div'+i,
-    //        event: eventForDate(i),
-    //        id: i,
-           
-    //      });
-    //     }
-    //     setDays(daysArr4)
-    //    },[events]);
-    //    useEffect(() => {
-    //     const daysArr5 =[];
-    //     for(let i = 1; i<45;i++){
-    //      daysArr5.push({
-    //        value: 'div'+i,
-    //        event: eventForDate(i),
-    //        id: i,
-           
-    //      });
-    //     }
-    //     setDays(daysArr5)
-    //    },[events]);
-
 
     return(
       <>
     <div id="container">
-    <h1>Grade de Salas</h1>
-
-    {/* <div id="weekdays">
-      
+    
+      <h1>Grade de Salas</h1>
+    <div id="weekdays">
+    
+      <div><img src="grade react\src\App\2.png" alt="logo ame"/></div>
       <div>Segunda</div>
       <div>Terça</div>
       <div>Quarta</div>
       <div>Quinta</div>
       <div>Sexta</div>
       
-    </div> */}
-    <div id="box">
+    </div>
+    <div id="localizacao">
+      <div className="salas">Salas<div className="nome"></div></div>
+      <div className="salas">01(b)<div className="nome">USG 1</div></div>
+      <div className="salas">01(c)<div className="nome">USG 2</div></div>
+      <div className="salas">01(f)<div className="nome">USG 3</div></div> 
+      <div className="salas">01(d)<div className="nome">Mamografia</div></div>
+      <div className="salas">01(e)<div className="nome">Raio X</div></div>
+      <div className="salas">02<div className="nome">Fisioterapia</div></div>
+      <div className="salas">07<div className="nome">ECG / Mapa / Holter</div></div>
+      <div className="salas">08<div className="nome"></div></div> 
+      <div className="salas">09<div className="nome">Curativo</div></div>
+      <div className="salas">10<div className="nome"></div></div>
+      <div className="salas">11<div className="nome">Cons. / EEG</div></div>
+      <div className="salas">12<div className="nome"></div></div>
+      <div className="salas">13<div className="nome"></div></div> 
+      <div className="salas">14<div className="nome">Teste Ergométrico</div></div>
+      <div className="salas">15<div className="nome"></div></div>
+      <div className="salas">16<div className="nome"></div></div>
+      <div className="salas">17<div className="nome">Nutrição</div></div>
+      <div className="salas">18<div className="nome">Psicologia</div></div> 
+      <div className="salas">19<div className="nome">Cons. / ENMG</div></div>
+      <div className="salas">20<div className="nome">Urologia</div></div>
+      <div className="salas">21<div className="nome">Campimetria</div></div>
+      <div className="salas">22(a)<div className="nome">Oftalmo 1</div></div>
+      <div className="salas">22(b)<div className="nome">Oftalmo 2</div></div> 
+      <div className="salas">23<div className="nome">Cons. / Espiro.</div></div>
+      <div className="salas">25<div className="nome">Enfermagem</div></div>
+      <div className="salas">26<div className="nome">Fono / Audio</div></div>
+      <div className="salas">27<div className="nome"></div></div>
+      <div className="salas">28<div className="nome"></div></div> 
+      <div className="salas">29<div className="nome"></div></div>
+      <div className="salas">30<div className="nome"></div></div>
+      <div className="salas">31<div className="nome"></div></div>
+      <div className="salas">32<div className="nome"></div></div>
+      <div className="salas">33<div className="nome"></div></div> 
+      <div className="salas">34<div className="nome">Acupuntura</div></div>
+      <div className="salas">35<div className="nome">Otorrino</div></div>
+      <div className="salas">36(a)<div className="nome">Bronco / Naso / Endoscopia</div></div>
+      <div className="salas">36(c)<div className="nome">Colono / Endoscopia</div></div>
+      <div className="salas">36(e)<div className="nome">Procedimentos</div></div> 
+      
+      
+      </div>
+
     <div id="calendar">
-      <div className="weekdays">Salas</div>
         {days.map((d, index) =>(
             <Day
             key = {index}
@@ -144,87 +110,15 @@ export const App = () => {
             }
             />
         ))}
-    </div>
-    <div id="calendar1">  
-    <div className="weekdays">Segunda-Feira</div>    
-        {days1.map((d, index) =>(
-            <Day1
-            key = {index}
-            day = {d}
-            onClick={() =>{                
-              setClicked(d.id);
-                
-            }
-            }
-            />
-        ))}
-    </div>
-    <div id="calendar2">   
-    <div className="weekdays">Quarta-Feira</div>      
-        {days.map((d, index) =>(
-            <Day
-            key = {index}
-            day = {d}
-            onClick={() =>{                
-              setClicked(d.id);
-                
-            }
-            }
-            />
-        ))}
-    </div>
-    <div id="calendar3">   
-    <div className="weekdays">Quarta-Feira</div>      
-        {days.map((d, index) =>(
-            <Day
-            key = {index}
-            day = {d}
-            onClick={() =>{                
-              setClicked(d.id);
-                
-            }
-            }
-            />
-        ))}
-    </div>
-    <div id="calendar4">  
-    <div className="weekdays">Quinta-Feira</div>       
-        {days.map((d, index) =>(
-            <Day
-            key = {index}
-            day = {d}
-            onClick={() =>{                
-              setClicked(d.id);
-                
-            }
-            }
-            />
-        ))}
-    </div>
-    <div id="calendar5">    
-    <div className="weekdays">Sexta-Feira</div>    
-        {days.map((d, index) =>(
-            <Day
-            key = {index}
-            day = {d}
-            onClick={() =>{                
-              setClicked(d.id);
-                
-            }
-            }
-            />
-        ))}
-    </div>
     </div>
   </div>
   {
-    clicked && !eventForDate(clicked) && 
+    clicked && !eventForDate(clicked) &&
     <NewEventModal 
     onClose={() => setClicked(null)}
     onSave={
       title =>{
         setEvents([...events, {title, id:clicked}]);
-        setEvents1([...events1, {title, id:clicked}]);
         
         setClicked(null);
       }
@@ -239,37 +133,10 @@ export const App = () => {
           onClose={() => setClicked(null)}
           onDelete={() => {
             setEvents(events.filter(e => e.id !== clicked));
-            setEvents1(events1.filter(e => e.id !== clicked));
             setClicked(null);
           }}
         />
       }
-       {/* {
-    clicked && !eventForDate1(clicked) &&
-    <NewEventModal 
-    onClose={() => setClicked(null)}
-    onSave={
-      title =>{
-        
-        setEvents1([...events1, {title, id:clicked}]);
-        
-        setClicked(null);
-      }
-    }
-    />
-  }
-  {
-        clicked && eventForDate1(clicked) &&
-        <DeleteEventModal 
-          eventText={eventForDate1(clicked).title}
-          onClose={() => setClicked(null)}
-          onDelete={() => {
-            
-            setEvents1(events1.filter(e => e.id !== clicked));
-            setClicked(null);
-          }}
-        />
-      } */}
   </>
   );
 };
